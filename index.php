d727a9f93efc253f73baafbd9eefb2d04e4d58a9
jamilahhh21 <jamilahhhibrahim96@gmail.com>
2021-10-04 12:49:31 +0800

    sehingga operators

--- index.php
+++ index.php
@@ -17,12 +17,43 @@
     */
 
     $namapelajar = 'Jamilah';
-    echo "<h2>Hello $namapelajar</h2>"; #output
+    echo "<h2>Hello $namapelajar !</h2>"; #output
 
     $hadir = false;
     $umur = 20;
     $nokp = "010121-02-0980";
 
+    $x = 10;
+    $x /= 5;
+    
+    echo "<h2>$x</h2>";
+
+    $x = 100;
+    $y = 80;
+
+    if ($x == 100 or $y == 80) {
+        echo "<h2>Hello World</h2>";
+    } else {
+        echo "<h2>Tak dak apa</h2>";
+    }
+
     ?>
+
+    <p>
+        <?php
+        echo 20 . 21;
+        ?>
+    </p>
+
+    <h2>
+        <?php
+        $bandar = 'Alor Setar';
+        $baru = '';
+        for($x = 0; $x < strlen($bandar); $x++) {
+            $baru .= $bandar[$x] . '-';
+        }
+        echo $baru;
+        ?>
+    </h2>
 </body>
 </html>
